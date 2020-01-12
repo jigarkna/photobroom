65278eef5e6d5792c5d4673f98c231cb9756e07e
Michał Walenciak <Kicer86@gmail.com>
2020-01-12 10:32:42 +0100

    instead of complex logic - embed dlib as subproject

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,3 @@ class PhotoBroomConan(ConanFile):
     requires = "Exiv2/0.27@piponazo/stable", "OpenLibrary/2.3@kicer/stable"
     generators = "cmake"
     default_options = {"Exiv2:xmp": False}
-
-    def configure(self):
-        if self.settings.os == "Linux":
-            self.requires.add("dlib/19.19@omaralvarez/stable")
